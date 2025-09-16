f41e56a0879748b64450e414cf77b782759f67e6
Clonix <syghwan@gmail.com>
2025-09-16 14:26:33 +0900

    In Game Scene에 오목판 오브젝트 추가

--- Assets/02.Script/Scene/InGameScene.cs
+++ Assets/02.Script/Scene/InGameScene.cs
@@ -2,4 +2,19 @@ using UnityEngine;
 
 public class InGameScene : Scene
 {
+    [SerializeField] GameObject InGameObjects;
+
+    public override void ActiveScene(bool active)
+    {
+        base.ActiveScene(active);
+        if (InGameObjects != null)
+        {
+            InGameObjects.SetActive(active);
+        }
+        else
+        {
+            Debug.LogAssertion($"<color=orange>연결된 GameObject가 없음</color>");
+        }
+
+    }
 }
