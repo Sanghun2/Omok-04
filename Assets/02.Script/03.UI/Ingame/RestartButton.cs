84b91b55becc5a4d70ab068449b5bf6573bcd594
lshzang86 <lshzang86@naver.com>
2025-09-18 16:58:49 +0900

    Restart Button 임시 커밋

--- Assets/02.Script/03.UI/Ingame/RestartButton.cs
+++ Assets/02.Script/03.UI/Ingame/RestartButton.cs
@@ -1,33 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RestartButton : MonoBehaviour
+public class RestartButton : ButtonBase
 {
-    [SerializeField] private Button restartButton;
     [SerializeField] private GameObject gameOverUI;
 
-    void Start()
-    {
-        if (restartButton != null)
-        {
-            restartButton.onClick.AddListener(() =>
-            {
-                Managers.Game.RestartLastGame();
+    protected override void ButtonAction() {
+        Restart();
+    }
 
-                // UI 재초기화
-                var ui = Object.FindObjectOfType<IngameUIController>();
-                if (ui != null)
-                {
-                    ui.ResetTurnUI();   // 턴 체크 초기화
-                    ui.InitUI();        // 타이머 & 이벤트 다시 연결
-                }
-                else
-                {
-                    Debug.Log("ui is null!");
-                }
 
-                    gameOverUI.SetActive(false);
-            });
+    private void Restart() {
+        Managers.Game.RestartLastGame();
+        Managers.Time.GetTimer().SetTimeAsDefault();
+
+        // UI 재초기화
+        var ui = Object.FindObjectOfType<IngameUIController>();
+        if (ui != null) {
+            ui.ResetTurnUI();   // 턴 체크 초기화
+            ui.InitUI();        // 타이머 & 이벤트 다시 연결
         }
+        else {
+            Debug.Log("ui is null!");
+        }
+
+        gameOverUI.SetActive(false);
     }
 }
