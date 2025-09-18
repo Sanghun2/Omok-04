3deef4577a8d4fa16ea58cce572d04d47ff479b8
jungeun-l <jungeunyi02@gmail.com>
2025-09-18 16:23:59 +0900

    로컬모드 체크턴
    - 재시작 UI 뜨면 초기화가 안됨

--- Assets/02.Script/03.UI/Ingame/RestartButton.cs
+++ Assets/02.Script/03.UI/Ingame/RestartButton.cs
@@ -13,6 +13,16 @@ public class RestartButton : MonoBehaviour
             restartButton.onClick.AddListener(() =>
             {
                 Managers.Game.RestartLastGame();
+                Managers.Board.InitBoard(); // 보드 ui 초기화
+
+                // UI 재초기화
+                var ui = Object.FindObjectOfType<IngameUIController>();
+                if (ui != null)
+                {
+                    ui.ResetTurnUI();   // 턴 체크 초기화
+                    ui.InitUI();        // 타이머 & 이벤트 다시 연결
+                }
+
                 gameOverUI.SetActive(false);
             });
         }
